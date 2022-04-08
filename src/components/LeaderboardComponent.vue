4c1b21a73b4808a29aaf409ea9eec1329b266e24
Dave-Tremblay <davetix21@hotmail.com>
2022-04-08 11:45:52 -0400

    Push pour Zack

---
+++ src/components/LeaderboardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid">
+    <div id="mainDiv" class="w-50 mx-auto">
+      <h2 class="text-center">e</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+#mainDiv {
+  background-color: rgb(227, 242, 253);
+}
+</style>
---
+++ src/views/Score.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Leaderboard />
+  </div>
+</template>
+
+<script>
+import Leaderboard from '@/components/LeaderboardComponent.vue'
+
+export default {
+  name: 'Score',
+  components: { Leaderboard }
+}
+</script>
+<style lang="css" scoped></style>
