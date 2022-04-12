4984b358f8ec901ff83ab84f4ba9748ca8d637bd
Zackary G Tremblay <zackarygtremb@gmail.com>
2022-04-12 16:40:46 -0400

    Squashed commit of the following:
    commit d13a603759b683804748d23caef6289557ce64f5
    Author: Zackary G Tremblay <zackarygtremb@gmail.com>
    Date:   Tue Apr 12 16:39:12 2022 -0400
    
        Reworked visuals
    
    commit 33274ba7e8ed22aa2c682711bf65fdfff4ebff68
    Merge: 1faaea7 6a8591d
    Author: Zackary G Tremblay <zackarygtremb@gmail.com>
    Date:   Tue Apr 12 16:12:40 2022 -0400
    
        Merge branch 'master' into PlayerStats
    
    commit 1faaea7149075161d86486c54b5820ad7d2df057
    Author: Zackary G Tremblay <zackarygtremb@gmail.com>
    Date:   Tue Apr 12 11:41:33 2022 -0400
    
        removed code for controller

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -2,15 +2,15 @@
   <div class="container-fluid">
     <b-row>
       <PlayerComponent class="col-6" />
-      <EnnemyComponent class="col-6" />
+      <EnemyComponent class="col-6" />
     </b-row>
   </div>
 </template>
 
 <script>
 import PlayerComponent from '../components/PlayerStatsComponent.vue'
-import EnnemyComponent from '../components/EnemyStatsComponent.vue'
-export default { components: { PlayerComponent, EnnemyComponent } }
+import EnemyComponent from '../components/EnemyStatsComponent.vue'
+export default { components: { PlayerComponent, EnemyComponent } }
 </script>
 
 <style lang="scss" scoped></style>
