2065714c7af6d4794ce7702073a37327ed86379f
Dave-Tremblay <davetix21@hotmail.com>
2022-04-12 16:19:14 -0400

    enemyCreated(without emitter)

---
+++ src/components/EnemyStatsComponent.vue
@@ -0,0 +1,81 @@
+<template>
+  <b-container id="mainDiv" class="rounded">
+    <b-row>
+      <b-col id="name" class="col-12 rounded"
+        ><p style="color:#fff">
+          {{ this.enemy.name }}
+        </p></b-col
+      >
+      <p style="color:#42b983" class="col-6">{{ this.rank }}</p>
+      <p style="color:#42b983" class="col-6">{{ this.enemy.credit }} CG</p>
+      <p style="color:#42b983" class="col-12">{{ this.enemy.ship.name }}</p>
+      <b-container
+        id="progressbar"
+        class="rounded progress-bar-warning progress-bar-striped progress-bar-animated"
+      >
+        <b-row>
+          <b-progress-bar
+            class="rounded bg-success progress-bar-striped progress-bar-animated"
+            :max="maxHealth"
+            :value="currentHealth"
+            :label="`${((currentHealth / maxHealth) * 100).toFixed(2)}%`"
+          ></b-progress-bar>
+        </b-row>
+      </b-container>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import { enemyService } from '../services/enemyService.js'
+export default {
+  data () {
+    return {
+      enemy: {},
+      rank: '',
+      maxHealth: 100,
+      currentHealth: 100
+    }
+  },
+  async created () {
+    this.enemy = await enemyService.getRandomCharacter()
+    switch (this.enemy.experience) {
+      case 1:
+        this.rank = 'Beginner'
+        break
+      case 2:
+        this.rank = 'Novice'
+        break
+      case 3:
+        this.rank = 'Experienced'
+        break
+      case 4:
+        this.rank = 'Master'
+        break
+    }
+  },
+  methods: {
+    was_attacked: function (damage) {
+      this.currentHealth -= damage
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#mainDiv {
+  background-color: rgb(32, 35, 37);
+  color: rgb(66, 185, 131);
+  padding-bottom: 20px;
+}
+
+#name {
+  background-color: rgb(0, 171, 34);
+  padding-top: 1%;
+  margin-bottom: 2%;
+}
+
+p {
+  font-size: 20px;
+}
+</style>
--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <PlayerStatsComponent />
+  <div class="container-fluid">
+    <b-row>
+      <PlayerComponent class="col-6" />
+      <EnnemyComponent class="col-6" />
+    </b-row>
   </div>
 </template>
 
 <script>
-import PlayerStatsComponent from '../components/playerStatsComponent.vue'
-export default { components: { PlayerStatsComponent } }
+import PlayerComponent from '../components/PlayerStatsComponent.vue'
+import EnnemyComponent from '../components/EnemyStatsComponent.vue'
+export default { components: { PlayerComponent, EnnemyComponent } }
 </script>
 
 <style lang="scss" scoped></style>
