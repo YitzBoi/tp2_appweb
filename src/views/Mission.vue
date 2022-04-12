34409f3acd8712a882dfd975ce369f0898c6138e
Dave-Tremblay <davetix21@hotmail.com>
2022-04-12 17:03:00 -0400

    fixed (maybe)

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import PlayerStatsComponent from '../components/PlayerStatsComponent.vue'
+import PlayerStatsComponent from '../components/PPPPPlayerStatsComponent.vue'
 import EnemyStatsComponent from '../components/EnemyStatsComponent.vue'
 import MissionInfosComponent from '../components/MissionInfosComponent.vue'
 import ActionsComponent from '../components/ActionsComponent.vue'
