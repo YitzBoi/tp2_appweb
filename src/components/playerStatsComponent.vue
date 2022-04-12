4c4a2724e2b243ea3f4d7803a7c417c93e9039c9
Zackary G Tremblay <zackarygtremb@gmail.com>
2022-04-12 09:14:14 -0400

    Cleaner again!!!

--- src/components/playerStatsComponent.vue
+++ src/components/playerStatsComponent.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="mainDiv" class="container-fluid row">
-    <p style="color:#42b983" class="col-12">{{ this.playerName }}</p>
-    <p style="color:#42b983" class="col-6">{{ this.playerRank }}</p>
-    <p style="color:#42b983" class="col-6">{{ this.playerCredit }} CG</p>
-    <p style="color:#42b983" class="col-12">{{ this.ship.name }}</p>
-    <b-container id="progressbar" class="rounded">
-      <b-row>
-        <b-progress-bar
-          class="rounded bg-success progress-bar-striped progress-bar-animated"
-          :max="maxHealth"
-          :value="currentHealth"
-          :label="`${((currentHealth / maxHealth) * 100).toFixed(2)}%`"
-        ></b-progress-bar>
-      </b-row>
-    </b-container>
-  </div>
+  <b-container id="mainDiv" class="rounded">
+    <b-row>
+      <b-col id="name" class="col-12 rounded"
+        ><p style="color:#fff">
+          {{ this.playerName }}
+        </p></b-col
+      >
+      <p style="color:#42b983" class="col-6">{{ this.playerRank }}</p>
+      <p style="color:#42b983" class="col-6">{{ this.playerCredit }} CG</p>
+      <p style="color:#42b983" class="col-12">{{ this.ship.name }}</p>
+      <b-container
+        id="progressbar"
+        class="rounded progress-bar-warning progress-bar-striped progress-bar-animated"
+      >
+        <b-row>
+          <b-progress-bar
+            class="rounded bg-success progress-bar-striped progress-bar-animated"
+            :max="maxHealth"
+            :value="currentHealth"
+            :label="`${((currentHealth / maxHealth) * 100).toFixed(2)}%`"
+          ></b-progress-bar>
+        </b-row>
+      </b-container>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -44,10 +53,16 @@ export default {
 #mainDiv {
   background-color: rgb(32, 35, 37);
   color: rgb(66, 185, 131);
-  border-radius: 20px;
-  padding-bottom: 10px;
+  padding-bottom: 20px;
 }
-#progressbar {
-  background-color: white;
+
+#name {
+  background-color: rgb(0, 171, 34);
+  padding-top: 1%;
+  margin-bottom: 2%;
+}
+
+p {
+  font-size: 20px;
 }
 </style>
