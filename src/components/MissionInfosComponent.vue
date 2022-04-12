25e221f8fbe6ecdee1995cf917e8954f7db21cd9
Dave-Tremblay <davetix21@hotmail.com>
2022-04-12 16:45:36 -0400

    Squashed commit of the following:
    commit 5a7e4d5c430d0948181442727dfae5abd965bb7f
    Author: Dave-Tremblay <davetix21@hotmail.com>
    Date:   Tue Apr 12 16:43:38 2022 -0400
    
        Created

---
+++ src/components/MissionInfosComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-container id="mainDiv">
+    <b-row>
+      <b-col id="name" class="col-12 rounded">
+        <p style="color:#fff">
+          Mission en cours
+        </p>
+      </b-col>
+      <b-col class="col-12">
+        <p style="color:#42b983" class="col-12">
+          {{ this.level }}/{{ this.maxLevel }}
+        </p>
+      </b-col>
+      <b-col class="col-12">
+        <p style="color:#42b983" class="col-12">
+          Objectif: Survivre à {{ this.maxLevel }} missions en obtenant le plus
+          de CG.
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      level: 1,
+      maxLevel: 5
+    }
+  },
+  async created () {},
+  methods: {
+    ennemyKilled: function () {
+      this.level++
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+#mainDiv {
+  background-color: rgb(32, 35, 37);
+  color: rgb(66, 185, 131);
+  padding-bottom: 20px;
+}
+
+#name {
+  background-color: rgb(0, 171, 34);
+  padding-top: 1%;
+  margin-bottom: 2%;
+}
+
+p {
+  font-size: 20px;
+}
+</style>
--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container-fluid">
     <b-row>
+      <MissionInfoComponent class="col-3" />
       <PlayerComponent class="col-6" />
       <EnemyComponent class="col-6" />
     </b-row>
@@ -10,7 +11,10 @@
 <script>
 import PlayerComponent from '../components/PlayerStatsComponent.vue'
 import EnemyComponent from '../components/EnemyStatsComponent.vue'
-export default { components: { PlayerComponent, EnemyComponent } }
+import MissionInfoComponent from '../components/MissionInfosComponent.vue'
+export default {
+  components: { PlayerComponent, EnemyComponent, MissionInfoComponent }
+}
 </script>
 
 <style lang="scss" scoped></style>
