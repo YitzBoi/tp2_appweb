5a7e4d5c430d0948181442727dfae5abd965bb7f
Dave-Tremblay <davetix21@hotmail.com>
2022-04-12 16:43:38 -0400

    Created

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container-fluid">
     <b-row>
+      <MissionInfoComponent class="col-3" />
       <PlayerComponent class="col-6" />
       <EnnemyComponent class="col-6" />
     </b-row>
@@ -10,7 +11,10 @@
 <script>
 import PlayerComponent from '../components/PlayerStatsComponent.vue'
 import EnnemyComponent from '../components/EnemyStatsComponent.vue'
-export default { components: { PlayerComponent, EnnemyComponent } }
+import MissionInfoComponent from '../components/MissionInfosComponent.vue'
+export default {
+  components: { PlayerComponent, EnnemyComponent, MissionInfoComponent }
+}
 </script>
 
 <style lang="scss" scoped></style>
